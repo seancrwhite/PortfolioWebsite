ae9230ae984b5277f35e99fb5ad80dc4eb41ddb6
seancrwhite <seancrwhite@gmail.com>
2018-03-31 19:36:41 -0600

    removed unnecessary packages

--- PortfolioWebsite/settings.py
+++ PortfolioWebsite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dlgin.apps.DlginConfig',
-    'bootstrap3',
     'bootstrap4',
     'librosa',
     'numpy',
